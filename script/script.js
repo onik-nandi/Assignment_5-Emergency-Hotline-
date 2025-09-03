90da4391f6b953801716beeac7332cd751ab3a11
onik-nandi <iamoniknandi@gmail.com>
2025-09-04 02:24:36 +0600

    copy function completed

--- script/script.js
+++ script/script.js
@@ -11,3 +11,19 @@ for (const heartElement of heart) {
     document.getElementById("heart-counts").innerText = totalHeartCount;
   });
 }
+// copy section
+const copyBtns = document.getElementsByClassName("copy-btn");
+for (const copyBtn of copyBtns) {
+  copyBtn.addEventListener("click", function () {
+    // console.log("clicked");
+    const copyCount = parseInt(document.getElementById("copy-count").innerText);
+    const totalCopyCount = copyCount + 1;
+    document.getElementById("copy-count").innerText = totalCopyCount;
+
+    const card = this.parentNode;
+
+    const copyText = card.getElementsByClassName("phone-number")[0].innerText;
+    navigator.clipboard.writeText(copyText);
+    alert("Number Copied: " + copyText);
+  });
+}
