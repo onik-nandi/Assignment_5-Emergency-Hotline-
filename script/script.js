2bca3ba0c8d76bf11df3a2a670374f0d88d90ae9
onik-nandi <iamoniknandi@gmail.com>
2025-09-04 02:27:25 +0600

    call button function completed

--- script/script.js
+++ script/script.js
@@ -27,3 +27,44 @@ for (const copyBtn of copyBtns) {
     alert("Number Copied: " + copyText);
   });
 }
+// all call button function
+const callBtns = document.getElementsByClassName("call-btn");
+const historyList = document.getElementById("history-list");
+const callCost = 20;
+
+for (const callBtn of callBtns) {
+  callBtn.addEventListener("click", function () {
+    // console.log("clicked");
+    const card = this.parentNode;
+    const moneyToken = parseInt(
+      document.getElementById("money-token").innerText
+    );
+    const now = new Date().toLocaleTimeString();
+    if (callCost <= moneyToken) {
+      {
+        const totalMoney = moneyToken - callCost;
+        document.getElementById("money-token").innerText = totalMoney;
+      }
+      const serviceType =
+        card.getElementsByClassName("service-type")[0].innerText;
+      const phoneNumber =
+        card.getElementsByClassName("phone-number")[0].innerText;
+      const serviceName =
+        card.getElementsByClassName("service-name")[0].innerText;
+      alert("📞 Calling " + serviceType + " " + phoneNumber);
+
+      const newDiv = document.createElement("div");
+      newDiv.innerHTML = `  <div class="call-card flex justify-between items-center md:mx-[25px] p-[16px] bg-[#FAFAFA] rounded-lg ">
+                  <div>
+                    <p class="text-[#111] font-semibold inter-font text-[18px]" >${serviceName}</p>
+                    <p class="hind-font text-[18px]">${phoneNumber}</p>
+                  </div>
+                  <p class="text-[#111] hind-font text-[18px]">${now}</p>
+              </div>
+      `;
+      historyList.prepend(newDiv);
+    } else {
+      alert(" Insufficient Tokens.Atleast need 20 tokens for call");
+    }
+  });
+}
